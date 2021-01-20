aa645b909960336a00339223c98e03b3c71feb29
Leonardo Gavaudan <leg2173@columbia.edu>
2021-01-20 14:01:44 +0100

    added permanent execution policy change

--- setupAnsible.ps1
+++ setupAnsible.ps1
@@ -1,5 +1,10 @@
+Set-ExecutionPolicy Bypass -Force
+
 powershell.exe -ExecutionPolicy ByPass -File ConfigureRemotingForAnsible.ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force
+
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 chocolatey install python3 -y
+
+Set-ExecutionPolicy Bypass -Force
+
