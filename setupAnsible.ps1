05fb5da282b933881ebdb73f92cc26c410267c0f
Leonardo Gavaudan <leg2173@columbia.edu>
2021-01-20 10:24:49 +0100

    added -y flag when installing python so it automatically accepts questions

--- setupAnsible.ps1
+++ setupAnsible.ps1
@@ -2,4 +2,4 @@ powershell.exe -ExecutionPolicy ByPass -File ConfigureRemotingForAnsible.ps1
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-chocolatey install python3
+chocolatey install python3 -y
