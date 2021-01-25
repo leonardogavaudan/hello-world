3c1552f9ca33f53b03bffbdc2d64baae38fe722c
Leonardo Gavaudan <leg2173@columbia.edu>
2021-01-25 18:39:27 +0100

    fixed flags on the main file exec

--- setupAnsible.ps1
+++ setupAnsible.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Force
 
-powershell.exe -ExecutionPolicy ByPass -File -ForceNewSSLCert ConfigureRemotingForAnsible.ps1
+powershell.exe -ExecutionPolicy ByPass -File ConfigureRemotingForAnsible.ps1 -ForceNewSSLCert -EnableCredSSP
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
