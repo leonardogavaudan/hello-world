5f6a09b67b5a8fe58e89abec174282788d4360f6
Leonardo Gavaudan <leg2173@columbia.edu>
2021-01-25 16:36:33 +0100

    praying

--- setupAnsible.ps1
+++ setupAnsible.ps1
@@ -1,11 +1,10 @@
 Set-ExecutionPolicy Bypass -Force
 
-powershell.exe -ExecutionPolicy ByPass -File ConfigureRemotingForAnsible.ps1
+powershell.exe -ExecutionPolicy ByPass -File -ForceNewSSLCert ConfigureRemotingForAnsible.ps1
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 chocolatey install python3 -y
-Enable-WSManCredSSP -Role Server -Force
 
 Set-ExecutionPolicy Bypass -Force
-
+Enable-WSManCredSSP -Role Server -Force
